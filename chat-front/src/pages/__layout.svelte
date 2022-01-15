e68cff0c946d75e2300adcf705dbf71db82ad5f3
LeeLeeLeeee <ijj1792@naver.com>
2022-01-15 07:08:18 +0000

    feat: 메인 디자인 잡기

--- chat-front/src/pages/__layout.svelte
+++ chat-front/src/pages/__layout.svelte
@@ -1,7 +1,12 @@
-<slot></slot>
+<div class="w-4/12 p-1 mx-auto h-full drop-shadow-md rounded-sm bg-white">
+	<slot></slot>
+</div>
 
 <style lang="postcss" global>
     @tailwind base;
     @tailwind components;
     @tailwind utilities;
+	body {
+		@apply font-noto-sans;
+	}
 </style>
--- chat-front/tailwind.config.cjs
+++ chat-front/tailwind.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/app.html', './src/**/*.svelte'],
   theme: {
     extend: {},
+    fontFamily: {
+      'noto-sans': ['noto-sans']
+    }
   },
   plugins: [],
 }
