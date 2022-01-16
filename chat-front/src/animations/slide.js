b8c2507fc1d8f4dd84151ccf91dea6647c2873a7
LeeLeeLeeee <ijj1792@naver.com>
2022-01-16 09:40:43 +0000

    feat: custom animation slide 변경

--- chat-front/src/animations/slide.js
+++ chat-front/src/animations/slide.js
@@ -1,11 +1,11 @@
 export const slide = (node, { duration }) => {
-    return {
-        duration,
-        css: (t, u) => {
-            return `
+	return {
+		duration,
+		css: (t, u) => {
+			return `
                 transform:translateY(${u * -20}px);
                 opacity:${t}
-            `
-        }
-    }
-}
+            `;
+		}
+	};
+};
