2a499fd943f0b43799c9bcb5c0686a19bfb799b5
LeeLeeLeeee <ijj1792@naver.com>
2022-01-16 06:10:45 +0000

    feat: Header 컴포넌트 네이밍 컨벤션 변경

---
+++ chat-front/src/components/common/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from '$components/common/Button';
+	import { setModalOpen, setModalTarget } from '$stores/modal';
+	import { userStore } from '$stores/user';
+
+	const createUserModalOpen = () => {
+		setModalTarget('create-user');
+		setModalOpen();
+	};
+</script>
+
+<div class='p-2 shadow-sm z-10 relative'>
+	{#if $userStore.username === ''}
+		<Button on:click={createUserModalOpen} main>계정 생성</Button>
+	{:else}
+		<Button>방 생성</Button>
+
+	{/if}
+</div>
