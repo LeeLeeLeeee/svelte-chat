84087d6d7fb941c6d884810392f57003f5d77029
LeeLeeLeeee <ijj1792@naver.com>
2022-01-19 23:41:01 +0900

    feat: 채팅창 말풍선 추가

---
+++ chat-front/src/components/chat/OtherChatBox.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let message = '';
+</script>
+<!-- TODO: ChatBox 하나로 추상화 합치기 -->
+<div class="flex justify-start">
+    <div id="thumbnail" />
+    <div>
+        <div class="text-xs text-right text-gray-500">다른 사람임</div>
+        <div class="relative text-sm max-w-sm bg-white p-2 rounded-md ml-2">
+            <span class="tri" />
+            {message}
+        </div>
+    </div>
+</div>
+
+<style>
+    #thumbnail {
+        width: 20px;
+        height: 20px;
+        @apply rounded-md;
+        @apply bg-yellow-500;
+    }
+    .tri {
+        position: absolute;
+        border-left: 7px solid transparent;
+        border-top: 7px solid;
+        border-bottom: 7px solid transparent;
+        border-right: 7px solid transparent;
+        left: -0.3rem;
+        top: 2px;
+        @apply border-t-white;
+        transform: rotate(358deg);
+    }
+</style>
--- chat-front/src/pages/chat/[id].svelte
+++ chat-front/src/pages/chat/[id].svelte
@@ -1,17 +1,26 @@
 <script>
 	import GoSignOut from 'svelte-icons/go/GoSignOut.svelte';
+	import GiBackup from 'svelte-icons/gi/GiBackup.svelte'
 	import Button from '$components/common/Button.svelte';
 	import Input from '$components/common/Input.svelte';
 	import { time } from '$stores/time';
+	import MyChatBox from '$components/chat/MyChatBox.svelte';
+import OtherChatBox from '$components/chat/OtherChatBox.svelte';
 </script>
 
-<div class="flex flex-col h-full">
+<div class="flex flex-col h-full rounded-lg">
 	<div class="border-b border-b-gray-300 p-2 flex justify-between items-center">
 		<span>{$time}</span>
 		<div class="icon"><GoSignOut /></div>
 	</div>
-	<div class="flex-1 bg-amber-50-50" />
-	<div class="border-t border-t-gray-400 p-2 flex">
+	<div class="flex flex-col gap-3 flex-1 bg-amber-50-50 pt-2 pb-2 pl-3 pr-3 bg-zinc-100">
+		<MyChatBox message={'안녕하세요'} />
+		<MyChatBox message={'혹시 성함이 어떻게 되시나요?'} />
+		<OtherChatBox message={'알아서 뭐하시게요??'} />
+	</div>
+	<div class="p-2 flex items-center">
+		<div class="icon"><GiBackup /></div>
+		<div class="border-l border-l-gray-400 m-1" />
 		<Input class="flex-1" />
 		<div class="border-l border-l-gray-400 m-1" />
 		<Button>전송</Button>
