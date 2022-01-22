2f63b3b554b72a6ed28f7724d9637d96dadee300
LeeLeeLeeee <ijj1792@naver.com>
2022-01-22 16:59:49 +0900

    feat: server websocket 초안 작업

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
-	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type messageType int
-
-type redisPool struct {
-	Pool *redis.Pool
-}
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:    1024,
+		WriteBufferSize:   1024,
+		EnableCompression: false,
+	}
+	myLogger = log.New(os.Stdout, "INFO: ", log.LstdFlags)
+)
 
 type responseFormat struct {
 	StatusCode int         `json:"status"`
@@ -29,40 +30,14 @@ type responseFormat struct {
 type clientInfo struct {
 	Conn       *websocket.Conn
 	ClientName string
-	Chatbox    string
+	RoomName   string
 }
-
-type messageFormat struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
-
-type conn struct {
-	Conn redis.Conn
-}
-
-var (
-	pool     redisPool
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:    1024,
-		WriteBufferSize:   1024,
-		EnableCompression: false,
-	}
-)
 
 func main() {
-	pool.Pool = redis.NewPool(func() (redis.Conn, error) {
-		return redis.Dial("tcp", ":6380")
-	}, 10)
-
-	defer pool.Pool.Close()
 
 	r := echo.New()
 	r.Use(middleware.Logger())
-	r.GET("/ws", connectClient)
-	r.GET("", func(c echo.Context) error {
-		return c.File("home.html")
-	})
+	r.POST("/connect", connectClient)
 	server := &http.Server{
 		Handler:      r,
 		ReadTimeout:  10 * time.Second,
@@ -75,16 +50,16 @@ func main() {
 
 func connectClient(c echo.Context) error {
 	username := c.QueryParam("name")
-	chatbox := c.QueryParam("chatbox")
-	if username == "" || chatbox == "" {
+	chatRoom := c.QueryParam("roomName")
+	if username == "" || chatRoom == "" {
 		return c.JSON(400, responseFormat{
 			StatusCode: 400,
-			Message:    "query name or chatbox must be filled",
+			Message:    "name and roomName must have a vlaue.",
 		})
 	}
 	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
-		fmt.Println(err)
+		myLogger.Println(err)
 		return c.JSON(500, responseFormat{
 			StatusCode: 500,
 			Message:    "Failed to create connection",
@@ -93,105 +68,30 @@ func connectClient(c echo.Context) error {
 	client := &clientInfo{
 		Conn:       conn,
 		ClientName: username,
-		Chatbox:    chatbox,
+		RoomName:   chatRoom,
 	}
 
-	go client.Run()
+	go client.Start()
 	return c.JSON(200, responseFormat{
 		StatusCode: 200,
-		Message:    "OK",
+		Message:    "Success",
 	})
 }
 
-func (c clientInfo) Run() {
-
-	ctx, cancel := context.WithCancel(context.Background())
+func (c clientInfo) Start() {
 	defer func() {
-		fmt.Println("selesai")
-		cancel()
-		c.Conn.Close()
-	}()
-	conn := pool.getConn()
-	defer conn.Conn.Close()
-	msgChan := make(chan []byte)
-	stopChan := make(chan bool)
-	go conn.Subscribe(ctx, msgChan, c.Chatbox)
-	go func() {
-		defer func() {
-			stopChan <- true
-		}()
-		conn := pool.getConn()
-		defer conn.Conn.Close()
-		for {
-			_, msg, err := c.Conn.ReadMessage()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			msg, err = marshalJSON(messageFormat{
-				Username: c.ClientName,
-				Message:  string(msg),
-			})
-			if err != nil {
-				return
-			}
-			if i, err := conn.Publish(c.Chatbox, string(msg)); err != nil {
-				return
-			} else if i != 1 {
-				fmt.Println(i)
-				continue
-			}
-		}
-	}()
 
-	for {
-		select {
-		case msg := <-msgChan:
-			c.Conn.WriteJSON(string(msg))
-		case <-stopChan:
-			return
-		}
-	}
-
-}
-
-func parseJSON(msg []byte) (res messageFormat, err error) {
-	err = json.Unmarshal(msg, &res)
-	return
-}
-
-func marshalJSON(data messageFormat) ([]byte, error) {
-	return json.Marshal(data)
-}
-
-func (p redisPool) getConn() *conn {
-	return &conn{
-		Conn: p.Pool.Get(),
-	}
-}
-
-func (c conn) Publish(key string, msg string) (interface{}, error) {
-	return c.Conn.Do("PUBLISH", key, msg)
-}
-
-func (c conn) Subscribe(ctx context.Context, msg chan []byte, key string) {
-	psc := redis.PubSubConn{Conn: c.Conn}
-	defer func() {
-		fmt.Println("selesai2")
-		psc.Close()
 	}()
 	for {
-		psc.Subscribe(key)
-		select {
-		case <-ctx.Done():
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
 			return
-		default:
-			switch v := psc.Receive().(type) {
-			case redis.Message:
-				msg <- v.Data
-			}
 		}
-
+		if err := c.Conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
 	}
 
 }
