bc407d385093768ce67dd4043f40a943bdffb930
LeeLeeLeeee <ijj1792@naver.com>
2022-01-23 23:40:54 +0900

    feat: socket hub 구성

--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-)
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:    1024,
-		WriteBufferSize:   1024,
-		EnableCompression: false,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
 )
 
 type responseFormat struct {
@@ -28,16 +15,13 @@ type responseFormat struct {
 	Data       interface{} `json:"data,omitempty"`
 }
 
-type clientInfo struct {
-	Conn       *websocket.Conn
-	ClientName string
-	RoomName   string
+type userInfo struct {
+	Name string `json:"name"`
 }
 
-type userInfo struct {
-	Name     string `json:"name"`
-	RoomName string `json:"roomName"`
-}
+var (
+	hub = newHub()
+)
 
 func main() {
 
@@ -62,47 +46,23 @@ func connectClient(c echo.Context) error {
 			Message:    "Failed to bind",
 		})
 	}
-	if u.Name == "" || u.RoomName == "" {
+	if u.Name == "" {
 		return c.JSON(400, responseFormat{
 			StatusCode: 400,
 			Message:    "name and roomName must have a vlaue.",
 		})
 	}
-	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	err := connectWs(hub, c.Response(), c.Request())
+
 	if err != nil {
-		fmt.Println(err)
 		return c.JSON(500, responseFormat{
 			StatusCode: 500,
 			Message:    "Failed to create connection",
 		})
 	}
-	client := &clientInfo{
-		Conn:       conn,
-		ClientName: u.Name,
-		RoomName:   u.RoomName,
-	}
 
-	go client.Start()
 	return c.JSON(200, responseFormat{
 		StatusCode: 200,
 		Message:    "Success",
 	})
-}
-
-func (c clientInfo) Start() {
-	defer func() {
-		c.Conn.Close()
-	}()
-	for {
-		messageType, p, err := c.Conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if err := c.Conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-
 }
