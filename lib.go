254912eb9f69d8c0c8b9096e3de20f5afb678565
LeeLeeLeeee <ijj1792@naver.com>
2022-02-02 20:33:45 +0900

    chatManager 추가, lib에 socket 연동 로직 이동

--- lib.go
+++ lib.go
@@ -1,6 +1,22 @@
 package main
 
-import "reflect"
+import (
+	"errors"
+	"net/http"
+	"reflect"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    1024,
+	WriteBufferSize:   1024,
+	EnableCompression: false,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 /* slice lib */
 func FindIndex(slice interface{}, f func(value interface{}) bool) int {
@@ -22,4 +38,12 @@ func RemoveItemOfSlice(slice interface{}, index int) interface{} {
 		return reflect.Append(value.Slice(0, index), value.Slice(index+1, s.Len()))
 	}
 	return nil
+}
+
+func ConnectWebSocket(w *echo.Response, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, errors.New("connect fail")
+	}
+	return conn, nil
 }
