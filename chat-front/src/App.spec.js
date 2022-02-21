fbcc97a55466c000044b9b851befb36d87ef881d
LeeLeeLeeee <ijj1792@naver.com>
2022-02-21 23:39:43 +0900

    feat: Home render 테스트 코드 작성

--- chat-front/src/App.spec.js
+++ chat-front/src/App.spec.js
@@ -1,8 +1,41 @@
 import { render, screen } from '@testing-library/svelte';
+import { fireEvent } from '@testing-library/svelte/node_modules/@testing-library/dom';
 import App from './pages';
+import CreateUserModal from './components/home/modals/CreateUserModal';
 
-test("says 'hello world!'", () => {
-    render(App);
-    const node = screen.queryByText("Hello world!");
-    expect(node).not.toBeNull();
-})
+
+describe("case-1 home render", () => {
+    
+    test("case-1-1 home has a create button", () => {
+        render(App);
+        const createButton = screen.queryByText("계정 생성");
+        expect(createButton).not.toBeNull();
+    })
+
+    test("case-1-2 home has a drop-down list", () => {
+        render(App);
+        const dropdown = screen.queryByText("생성된 유저 목록");
+        expect(dropdown).not.toBeNull();
+    })
+});
+
+describe("case-2 render create user modal", () => {
+    
+    test("case-2-1 check title has been rendered", () => {
+        render(CreateUserModal);
+        const header = screen.queryByText("계정 생성");
+        expect(header).not.toBeNull();
+    })
+
+    test("case-2-2 check label has been rendered", () => {
+        render(CreateUserModal);
+        const label = screen.queryByText("이름");
+        expect(label).not.toBeNull();
+    })
+
+    test("case-2-3 check button has been rendered", () => {
+        render(CreateUserModal);
+        const button = screen.getByText("생성");
+        expect(button).not.toBeNull();
+    })
+});
