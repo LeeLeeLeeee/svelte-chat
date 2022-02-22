24cea6133bd0652867e418b75d8e5bc0510265f7
LeeLeeLeeee <ijj1792@naver.com>
2022-02-22 22:14:24 +0900

    feat: Jest Modal, axios mock 함수로 변경

--- chat-front/src/App.spec.js
+++ chat-front/src/App.spec.js
@@ -2,40 +2,54 @@ import { render, screen } from '@testing-library/svelte';
 import { fireEvent } from '@testing-library/svelte/node_modules/@testing-library/dom';
 import App from './pages';
 import CreateUserModal from './components/home/modals/CreateUserModal';
+import { setModalClose, setModalOpen, setModalTarget } from './stores/modal';
+import axios from 'axios';
 
-
+jest.mock('axios');
 describe("case-1 home render", () => {
-    
+    beforeAll(() => {
+        const response = { status: 200, data: { data: [] }};
+        axios.get.mockResolvedValue(response);
+    })
     test("case-1-1 home has a create button", () => {
+        
         render(App);
         const createButton = screen.queryByText("계정 생성");
         expect(createButton).not.toBeNull();
-    })
+    });
 
     test("case-1-2 home has a drop-down list", () => {
         render(App);
         const dropdown = screen.queryByText("생성된 유저 목록");
         expect(dropdown).not.toBeNull();
-    })
+    });
 });
 
 describe("case-2 render create user modal", () => {
-    
+    beforeAll(() => {
+        setModalTarget('create-user');
+        setModalOpen();
+    });
+
     test("case-2-1 check title has been rendered", () => {
         render(CreateUserModal);
         const header = screen.queryByText("계정 생성");
         expect(header).not.toBeNull();
-    })
+    });
 
     test("case-2-2 check label has been rendered", () => {
         render(CreateUserModal);
         const label = screen.queryByText("이름");
         expect(label).not.toBeNull();
-    })
+    });
 
     test("case-2-3 check button has been rendered", () => {
         render(CreateUserModal);
         const button = screen.getByText("생성");
         expect(button).not.toBeNull();
+    });
+
+    afterAll(() => {
+        setModalClose();
     })
 });
